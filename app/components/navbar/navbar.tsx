3cc16987564055e5d6c6b03569b282016df7a6d9
Edsel <edsel.lorejo@msunaawan.edu.ph>
2025-11-11 10:16:39 +0800

    Lorejo Initial Commit

---
+++ app/components/navbar/navbar.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import Link from "next/link";
+import SearchFilters from "./SearchFilters";
+import AddPropertyButton from "./AddPropertyButton";
+import UserNav from "./UserNav";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full fixed top-0 left-0 py-6 border-b border-gray-200 bg-white z-10">
+      <div className="max-w-[1500px] mx-auto px-6">
+        <div className="flex justify-between items-center">
+          
+          {/* Logo */}
+          <Link href="/">
+            <Image
+              src="/logo.png"
+              alt="DjangoBnb logo"
+              width={180}
+              height={38}
+            />
+          </Link>
+
+          {/* Search Filters */}
+          <div className="flex space-x-6">
+            <SearchFilters />
+          </div>
+
+          {/* Add Property + User Menu */}
+          <div className="flex items-center space-x-6">
+            <AddPropertyButton />
+            <UserNav />
+          </div>
+
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
