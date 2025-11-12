a5af0ec046ffa6e91c335030959b28df3976f56c
Edsel <edsel.lorejo@msunaawan.edu.ph>
2025-11-12 23:12:53 +0800

    inbox

---
+++ app/components/inbox/Conversation.tsx
@@ -0,0 +1,12 @@
+const Conversation = () => {
+    return (
+        <div className="px-6 py-4 cursor-pointer border border-gray-300 rounded-xl">
+            <p className="mb-6 text-xl">Edsel M. Lorejo</p>
+            <p className="text-red-500">
+                Got to Conversation
+            </p>
+        </div>
+    )
+}
+
+export default Conversation;
