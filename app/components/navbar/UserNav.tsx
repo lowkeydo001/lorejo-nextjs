65591dd31fd3a53ef3107d6526a566bd65fd26e0
Edsel <edsel.lorejo@msunaawan.edu.ph>
2025-11-13 08:39:41 +0800

    Log in Modal

--- app/components/navbar/UserNav.tsx
+++ app/components/navbar/UserNav.tsx
@@ -3,16 +3,18 @@
 
 import { useState } from "react";
 import MenuLink from "./MenuLink";
+import useLoginModal from "@/app/hooks/useLoginModal";
 
 
 
 
 const UserNav = () => {
+    const loginModal = useLoginModal();
     const [isOpen, setIsOpen] = useState(false)
     return (
         <div className="p-2 relative inline-block border  border-gray-300 rounded-full">
             <button
-                onClick={() => setIsOpen(!isOpen)}
+ onClick={() => setIsOpen(!isOpen)}
                 className="flex items-center">
                 <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -21,27 +23,35 @@ const UserNav = () => {
 
                 <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
- </svg>
+                </svg>
             </button>
 
 
             {isOpen && (
                 <div className="w-[220px] absolute top-[60px] right-0 bg-white border border-gray-300 rounded-xl shadow-md flex flex-col cursor-pointer overflow-hidden">
+
+
+
+
                     <MenuLink
-                        label="Login"
-                        onClick={() => console.log('Clicked button')}
+                        label='Log in'
+                        onClick={() => {
+                            loginModal.open();
+                            setIsOpen(false);
+                            console.log('Clicked button')
+                        }}
                     />
 
 
                     <MenuLink
-                        label='Sign up'
+                        label="Sign up"
                         onClick={() => console.log('Clicked button')}
                     />
                 </div>
             )}
         </div>
     )
-}
+    }
 
 
 export default UserNav;
