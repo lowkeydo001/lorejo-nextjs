8c845e65e00d7384c6541984769e58ea24d59537
Edsel <edsel.lorejo@msunaawan.edu.ph>
2025-11-22 14:40:13 +0800

    Get properties in the frontend and list them - EDSEL M. LOREJO

--- djangobnb/app/components/properties/PropertyListItem.tsx
+++ djangobnb/app/components/properties/PropertyListItem.tsx
@@ -1,28 +1,36 @@
-import Image from "next/image";
+import { PropertyType } from "./PropertyList";
 
 
-const PropertyListItem = () => {
+
+
+interface PropertyProps {
+    property: PropertyType,
+    markFavorite?: (is_favorite: boolean) => void;
+}
+
+
+const PropertyListItem: React.FC<PropertyProps> = ({
+    property,
+}) => {
     return (
         <div className="cursor-pointer">
             <div className="relative overflow-hidden aspect-square rounded-xl">
-                <Image
-                    fill
-                    src="/beach_1.jpeg"
-                    sizes="(max-width: 768px) 768px, (max-width: 1200px): 768px, 768px"
+                <img
+                    src={property.image_url}
                     className="hover:scale-110 object-cover transition h-full w-full"
                     alt="Beach house"
                 />
             </div>
 
 
-           <div className="mt-2">
-                <p className="text-lg font-bold">Property Name</p>
+            <div className="mt-2">
+                <p className="text-lg font-bold">{property.title}</p>
             </div>
 
 
             <div className="mt-2">
                 <p className="text-sm text-gray-700">
-                    <strong>$200</strong> per night
+                    <strong>{property.price_per_night}</strong> per night
                 </p>
             </div>
 
