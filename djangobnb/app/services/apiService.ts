75aa3e76941d652e74426f5d3a4d467ee30ed5e0
Edsel Lorejo <EDSEL M. LOREJO-edsel.lorejo@msunaawan.edu.ph>
2025-11-24 16:43:28 +0800

    Implement Authentication (Sign up) - EDSEL M. LOREJO

--- djangobnb/app/services/apiService.ts
+++ djangobnb/app/services/apiService.ts
@@ -3,15 +3,13 @@ const apiService = {
         console.log('get', url);
 
 
-        // const token = await getAccessToken();
-
-
         return new Promise((resolve, reject) => {
             fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
                 method: 'GET',
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json',
+                    // 'Authorization': `Bearer ${token}`
                 }
             })
                 .then(response => response.json())
@@ -23,6 +21,47 @@ const apiService = {
                     reject(error);
                 }))
         })
-    }
+    },
+
+
+
+
+    post: async function (url: string, data: any): Promise<any> {
+        console.log('post', url, data);
+
+
+
+
+        return new Promise((resolve, reject) => {
+            fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
+                method: 'POST',
+                body: data,
+                headers: {
+                    // 'Authorization': `Bearer`,
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then((json) => {
+                    console.log('Response:', json);
+
+
+
+
+                    resolve(json);
+                })
+                .catch((error => {
+                    reject(error);
+                }))
+        })
+    },
 }
+
+
+
+
+
+
+
+
 export default apiService;
