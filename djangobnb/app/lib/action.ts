75793306a2129f233aa1e2c13cf8c2b998287ac1
Lowkeydo001 <edsel.lorejo@msunaawan.edu.ph>
2025-11-25 14:09:48 +0800

    Implementation Authentication (Log-out) - EDSEL LOREJO

--- djangobnb/app/lib/action.ts
+++ djangobnb/app/lib/action.ts
@@ -33,3 +33,22 @@ export async function handleLogin(userId: string, accessToken: string, refreshTo
         path: '/'
     });
 }
+
+
+export async function resetAuthCookies() {
+    const cookieStore = await cookies();
+    cookieStore.set('session_userid', '', { maxAge: 0 });
+    cookieStore.set('session_access_token', '', { maxAge: 0 });
+    cookieStore.set('session_refresh_token', '', { maxAge: 0 });
+}
+
+
+//
+// Get data
+
+
+export async function getUserId() {
+    const cookieStore = await cookies();
+    const userId = cookieStore.get('session_userid')?.value
+    return userId ? userId : null
+}
