af72d7c977444ac56ad9d9129eda1856e60eb8b1
Edsel <edsel.lorejo@msunaawan.edu.ph>
2025-12-05 20:58:56 +0800

    Video 5 - Detail Page for Properties - EDSEL LOREJO

--- djangobnb/app/components/properties/ReservationSidebar.tsx
+++ djangobnb/app/components/properties/ReservationSidebar.tsx
@@ -1,11 +1,28 @@
-const ReservationSidebar = () => {
+export type Property = {
+  id: string;
+  guests: number;
+  price_per_night: number;
+}
+
+
+interface ReservationSidebarProps {
+  property: Property
+}
+
+
+const ReservationSidebar: React.FC<ReservationSidebarProps> = ({
+  property
+}) => {
   return (
     <aside className="mt-6 p-6 col-span-2 rounded-xl border border-gray-300 shadow-xl">
-      <h2 className="mb-5 text-2xl">$200 Per night</h2>
+      <h2 className="mb-5 text-2xl">${property.price_per_night} per night</h2>
+
+
+
 
       <div className="mb-6 p-3 border border-gray-400 rounded-xl">
         <label className="mb-2 block font-bold text-xs">Guests</label>
-        <select className="w-full -ml-1 text-sm">
+        <select className="w-full -ml-1 text-xm">
           <option value="">1</option>
           <option value="">2</option>
           <option value="">3</option>
@@ -13,28 +30,32 @@ const ReservationSidebar = () => {
         </select>
       </div>
 
-      <div className="w-full mb-6 py-6 text-center text-white bg-airbnb hover:bg-airbnb-dark rounded-xl cursor-pointer">
+
+      <div className="w-full mb-6 py-6 text-center text-white bg-airbnb hover:bg-airbnb-dark rounded-xl">
         <p className="text-lg font-bold">Book</p>
       </div>
 
-      <div className="mb-4 flex justify-between items-center">
+
+      <div className="mb-4 flex justify-between align-center">
         <p>$200 x 4 nights</p>
         <p>$800</p>
       </div>
 
-      <div className="mb-4 flex justify-between items-center">
+
+      <div className="mb-4 flex justify-between align-center">
         <p>Djangobnb Fee</p>
         <p>$40</p>
       </div>
-
       <hr />
 
-      <div className="mt-4 flex justify-between items-center font-bold">
+
+      <div className="mb-4 flex justify-between align-center font-bold">
         <p>Total</p>
         <p>$840</p>
       </div>
     </aside>
-  );
-};
-
+  )
+}
 export default ReservationSidebar;
+
+
--- djangobnb/next.config.ts
+++ djangobnb/next.config.ts
@@ -5,13 +5,20 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'http',
         hostname: '127.0.0.1',
         port: '8000',
-        pathname: '/**',
+        pathname: '/media/**',
       },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/media/**',
+      }
     ],
   },
 };
