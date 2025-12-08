65cd95b6ab6092e5b3c6017e098ade97f31d4cb4
Edsel <edsel.lorejo@msunaawan.edu.ph>
2025-12-08 14:20:59 +0800

    Video 6  - Make it possible to set property as favorite - EDSEL LOREJO

--- djangobnb/app/components/properties/PropertyList.tsx
+++ djangobnb/app/components/properties/PropertyList.tsx
@@ -11,6 +11,7 @@ export type PropertyType = {
     title: string;
     image_url: string;
     price_per_night: number;
+    is_favorite: boolean;
 }
 
 
@@ -21,10 +22,34 @@ interface PropertyListProps {
 
 
 const PropertyList: React.FC<PropertyListProps> = ({
-    landlord_id
-    // favorites
+    landlord_id,
+    favorites
 }) => {
     const [properties, setProperties] = useState<PropertyType[]>([]);
+
+
+    const markFavorite = (id: string, is_favorite: boolean) => {
+        const tmpProperties = properties.map((property: PropertyType) => {
+            if (property.id == id) {
+                property.is_favorite = is_favorite
+
+
+                if (is_favorite) {
+                    console.log('added to list of favorited propreties')
+                } else {
+                    console.log('removed from list')
+                }
+            }
+
+
+            return property;
+        })
+
+
+        setProperties(tmpProperties);
+    }
+
+
 
 
     let url = '/api/properties/';
@@ -36,10 +61,19 @@ const PropertyList: React.FC<PropertyListProps> = ({
 
 
     const getProperties = async () => {
-        const tmpProperties = await apiService.get(url)
+        const tmpProperties = await apiService.get(url);
 
 
-        setProperties(tmpProperties.data)
+        setProperties(tmpProperties.data.map((property: PropertyType) => {
+            if (tmpProperties.favorites.includes(property.id)) {
+                property.is_favorite = true
+            } else {
+                property.is_favorite = false
+            }
+
+
+            return property
+        }));
     };
 
 
@@ -55,7 +89,7 @@ const PropertyList: React.FC<PropertyListProps> = ({
                     <PropertyListItem
                         key={property.id}
                         property={property}
-                    // markFavorite={(is_favorite: any) => markFavorite(property.id, is_favorite)}
+                        markFavorite={(is_favorite: any) => markFavorite(property.id, is_favorite)}
                     />
                 )
             })}
@@ -65,4 +99,3 @@ const PropertyList: React.FC<PropertyListProps> = ({
 
 
 export default PropertyList;
-
--- djangobnb/app/components/properties/PropertyListItem.tsx
+++ djangobnb/app/components/properties/PropertyListItem.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { PropertyType } from "./PropertyList";
 import { useRouter } from "next/navigation";
+import FavoriteButton from "../FavoriteButton";
 
 
 interface PropertyProps {
@@ -10,24 +11,34 @@ interface PropertyProps {
 
 
 const PropertyListItem: React.FC<PropertyProps> = ({
-    property
+    property,
+    markFavorite
 }) => {
     const router = useRouter();
+
+
     return (
         <div
             className="cursor-pointer"
             onClick={() => router.push(`/properties/${property.id}`)}
-
-
         >
             <div className="relative overflow-hidden aspect-square rounded-xl">
                 <Image
-                    src={property.image_url}
-                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                     fill
+                    src={property.image_url}
+                    sizes="(max-width: 768px) 768px, (max-width: 1200px): 768px, 768px"
                     className="hover:scale-110 object-cover transition h-full w-full"
                     alt="Beach house"
                 />
+
+
+                {markFavorite && (
+                    <FavoriteButton
+                        id={property.id}
+                        is_favorite={property.is_favorite}
+                        markFavorite={(is_favorite) => markFavorite(is_favorite)}
+                    />
+                )}
             </div>
 
 
@@ -37,13 +48,11 @@ const PropertyListItem: React.FC<PropertyProps> = ({
 
 
             <div className="mt-2">
-                <p className="text-sm text-gray-700">
-                    <strong>{property.price_per_night}</strong> per night
-                </p>
+                <p className="text-sm text-gray-500"><strong>${property.price_per_night}</strong> per night</p>
             </div>
-
-
         </div>
     )
 }
+
+
 export default PropertyListItem;
