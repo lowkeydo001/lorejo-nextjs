ef3c9a051bad84bc86f5fd44e9be8b071bb997a7
Edsel <edsel.lorejo@msunaawan.edu.ph>
2025-12-15 19:40:37 +0800

    Video 7 -  Make it Possible to Connect  -  Edsel Lorejo

--- djangobnb/app/components/inbox/ConversationDetail.tsx
+++ djangobnb/app/components/inbox/ConversationDetail.tsx
@@ -1,22 +1,56 @@
 'use client';
+
+
+import { useEffect, useState, useRef } from "react";
 import CustomButton from "@/app/components/forms/CustomButton";
+import { ConversationType } from "@/app/inbox/page";
+import useWebSocket, { ReadyState } from "react-use-websocket";
 
 
 
 
-const ConversationDetail = () => {
+interface ConversationDetailProps {
+    token: string;
+    userId: string;
+    conversation: ConversationType;
+}
+
+
+const ConversationDetail: React.FC<ConversationDetailProps> = ({
+    userId,
+    token,
+    conversation
+}) => {
+
+
+    const myUser = conversation.users?.find((user) => user.id == userId)
+    const otherUser = conversation.users?.find((user) => user.id != userId)
+
+
+    const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(`ws://127.0.0.1:8000/ws/${conversation.id}/?token=${token}`, {
+        share: false,
+        shouldReconnect: () => true,
+    },
+    )
+
+
+    useEffect(() => {
+        console.log("Connection state changed", readyState);
+    }, [readyState]);
+
+
     return (
         <>
             <div className="max-h-[400px] overflow-auto flex flex-col space-y-4">
-                <div className="w-[80%] py-4 px-6 rounded-xl bg-gray-200">
-                    <p className="font-bold text-gray-500">Debug My Heart</p>
-                    <p className="text-gray-500">Hi, Musta man ka?.</p>
+                <div className="w-[80%]py-4 px-6 rounded-xl bg-gray-200">
+                    <p className="font-bold text-gray-500">Avelora Danstroam</p>
+                    <p className="text-gray-500">Hi, how are you? Its been a while since we hung out.</p>
                 </div>
 
 
                 <div className="w-[80%]py-4 ml-[20%] px-6 rounded-xl bg-blue-200">
-                    <p className="font-bold text-gray-500">Edsel M. Lorejo</p>
-                    <p className="text-gray-500">Okay ra uy, hehhee</p>
+                    <p className="font-bold text-gray-500">Erica Jane N. Bucol</p>
+                    <p className="text-gray-500">Well, Im good, mweheheheh. Yeah, its been a while! How have you been?</p>
                 </div>
             </div>
 
@@ -31,7 +65,7 @@ const ConversationDetail = () => {
 
                 <CustomButton
                     onClick={() => console.log('clicked')}
-                    label="Send" 
+                    label="Send"
                     className="w-[100px]"
                 />
 
